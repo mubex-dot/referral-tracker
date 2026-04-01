4dc1a2a2957aa745636042f0197affb0969612c6
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2026-04-01 02:07:36 +0100

    explicitly binding the app to 0.0.0.0

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { setupRoutes } from "./routers/apiRoutes.ts";
 import { loggerMiddleware } from "./middlewares/logger.ts";
 
 export const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10);
 
 app.use(loggerMiddleware);
 
@@ -11,6 +11,6 @@ app.use(express.json());
 
 setupRoutes(app);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
